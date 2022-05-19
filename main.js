e6d39c25852f6b06c9c8b871314dff76a723c950
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2022-05-19 15:08:57 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+let newAttraction = document.createElement('li');
+
+newAttraction.id = 'vespa';
+newAttraction.innerHTML = 'Rent a Vespa';
+
+document.getElementById('italy-attractions').appendChild(newAttraction);
